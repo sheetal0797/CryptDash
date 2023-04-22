44e3bf9ed50b2788ef6bbc7276b036e99e5eac46
SheetalAgarwal <Sheetal0797@gmail.com>
2023-04-22 22:45:35 +0530

    index files

---
+++ App.js
@@ -0,0 +1,32 @@
+import { makeStyles } from "@material-ui/core";
+import Alert from "./components/Alert";
+import { BrowserRouter, Route} from "react-router-dom";
+import './App.css'
+import Coinpage from "./components/Coinpage";
+import Header from "./components/Header";
+import Homepage from "./components/Homepage";
+
+function App() {
+  const useStyles=makeStyles(()=>({
+    App:{
+        backgroundColor:'#14161a',
+        color:'white',
+        minHeight:'100vh'
+    }
+  }));
+
+  const classes=useStyles();
+
+  return (
+    <BrowserRouter>
+    <div className={classes.App}>
+      <Header />
+      <Route path="/" component={Homepage} exact />
+      <Route path="/coins/:id" component={Coinpage} exact />
+    </div>
+    <Alert/>
+  </BrowserRouter>
+  );
+}
+
+export default App;

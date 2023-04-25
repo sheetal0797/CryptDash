0f51b0b30e2af6ec0b175c189686428183acbbff
SheetalAgarwal <Sheetal0797@gmail.com>
2023-04-25 23:19:33 +0530

    file added

---
+++ src/CryptoContext.js
@@ -0,0 +1,125 @@
+import axios from 'axios';
+import { auth, db } from "./firebase";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { CoinList } from './config/api';
+import { onSnapshot } from 'firebase/firestore';
+import { doc } from "firebase/firestore";
+
+const Crypto = createContext();
+
+const CryptoContext = ({ children }) => {
+  const [currency, setCurrency] = useState("INR");
+  const [symbol, setSymbol] = useState("₹");
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const  [user,setUser]=useState(null);
+  const [alert,setAlert]=useState({
+    open:false,
+    message:'',
+    type:'success'
+  });
+  const [watchlist, setWatchlist]=useState([]);
+
+  useEffect(()=>{
+    if(user){
+      console.log("user changed to ");
+      setAlert({
+        open: true,
+        message: `User changed to ${user}`,
+        type: "error",
+    });
+    console.log("for watchlist");
+    console.log(user);
+    fetch("http://localhost:5000/getwatchlist", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        email:user,
+      }),      
+    })
+      .then((res) => res.json())
+      .then((data) => {
+          if(data.watchlist){
+          setWatchlist(data.watchlist);
+          }
+          else
+          {
+            console.log("nothing in watchlist");
+          }
+      })
+
+
+
+
+    //   const coinRef=doc(db,'watchlist', user.uid);
+    // var unsubscribe=  onSnapshot(coinRef,coin=>{
+    //     if(coin.exists()){
+    //       setWatchlist(coin.data().coins);
+    //     }else{
+    //       console.log('nothis in watch list');
+    //     }
+    //   }
+    //   );
+    //   return ()=>{
+    //     unsubscribe();
+    //   }
+    }
+  },[user]);
+
+
+  // useEffect(()=>{
+  //   if(user){
+  //     const coinRef=doc(db,'watchlist', user.uid);
+  //   var unsubscribe=  onSnapshot(coinRef,coin=>{
+  //       if(coin.exists()){
+  //         setWatchlist(coin.data().coins);
+  //       }else{
+  //         console.log('nothis in watch list');
+  //       }
+  //     }
+  //     );
+  //     return ()=>{
+  //       unsubscribe();
+  //     }
+  //   }
+  // },[user]);
+
+  // useEffect(()=>{
+  //   onAuthStateChanged(auth,(user)=>{
+  //     if(user)setUser(user);
+  //     else setUser(null);
+  //   });
+  // },[]);
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const { data } = await axios.get(CoinList(currency));
+    console.log(data);
+
+    setCoins(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (currency === "INR") setSymbol("₹");
+    else if (currency === "USD") setSymbol("$");
+  }, [currency]);
+
+  return (
+    <Crypto.Provider value={{ currency, setCurrency, symbol,coins,loading,fetchCoins, alert,setAlert,user, setUser,watchlist, setWatchlist,}}>
+      {children}
+    </Crypto.Provider>
+  );
+};
+
+export default CryptoContext;
+
+export const CryptoState = () => {
+  return useContext(Crypto);
+};

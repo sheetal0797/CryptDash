e105624071474ec75d9fe0f67a1e489626584a5f
SheetalAgarwal <Sheetal0797@gmail.com>
2023-05-15 22:47:04 +0530

    ansible vault

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv');
 const { ConnectionOptions } = require('mongoose');
-const yenv = require('yenv')
+//const yenv = require('yenv')
 
-let env = yenv('env.yml')
+//let env = yenv('env.yml')
 
-// dotenv.config({ path: '.env' });
+ dotenv.config({ path: '.env' });
 
 const mongoOpts = {
     useNewUrlParser: true    
@@ -12,8 +12,8 @@ const mongoOpts = {
 
 const mongoConfig = {
     //url: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-    // url: `mongodb://${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`,
-    // url: `mongodb://mongo/${process.env.DB_NAME}`,
+     //url: `mongodb://${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`,
+    //url: `mongodb://mongo/${process.env.DB_NAME}`,
     url: `mongodb://mongo/${env.DB_NAME}`,
     configs: mongoOpts,
 }
